396a71cba78f9dc2fd766f3af1ab227e5030c247
ishanuc <ishanu.chattopadhyay@gmail.com>
2024-01-13 19:24:23 -0600

    updated

---
+++ build/lib/teomim/__init__.py
@@ -0,0 +1,20 @@
+"""                                                                                                                        
+# teomim                                                                                                                   
+                                                                                                                           
+Description:                                                                                                               
+    Digital twin for human disease risk. Generate realistic diagnostic histories for ptients nearly indistinguishable from real population using deep comorbidity signatures.                                                                  
+                                                                                                                           
+---                                                                                                                        
+                                                                                                                           
+Installation:                                                                                                              
+   ```                                                                                                                     
+      pip install teomim                                                                                                   
+   ```                                                                                                                                                                                                      
+Notes:                                                                                                                     
+   Models are contextual
+
+"""
+
+
+from .teomim import teomim
+
